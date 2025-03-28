832d76a85ee8dd971dd0619abd463f0518b670a8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-28 22:01:14 +0530

    feat: Added callback fn

--- lib/phx_cicd_automation/auth/auth_controller.ex
+++ lib/phx_cicd_automation/auth/auth_controller.ex
@@ -5,7 +5,20 @@ defmodule PhxCicdAutomation.Auth.AuthController do
   def request(conn, _params) do
     redirect(conn, external: GitHub.authorize_url())
   end
+  def callback(conn, %{"code" => code}) do
+    case GitHub.get_token(code) do
+      {:ok, token} ->
+        conn
+        |> put_session(:github_token, token.access_token)
+        |> put_flash(:info, "Successfully authenticated!")
+        |> redirect(to: "/repos")
 
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Authentication failed!")
+        |> redirect(to: "/")
+    end
+  end
 
 
 end
